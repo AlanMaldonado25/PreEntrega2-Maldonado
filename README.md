65a767c3cba53b1f431083ef3be03c774c292718
Alan Maldonado <alanmaldo.ct@gmail.com>
2023-08-28 23:21:01 -0300

    Agregando detalles al readme y agregando condicional para ingresos de numeros

--- README.md
+++ README.md
@@ -18,7 +18,37 @@ Para esta entrega el programa tiene que contar con lo siguiente:
 
 7.Metodos para los arrays ✅
 
-## _A continuacion los links para conocer el proyecto._
+# _A continuacion voy a explicar un poco el funcionamiento del programa.
+
+## 1._Arrays para almacenar datos:_
++ 'gastos': Un array que almacena los gastos junto con sus descripciones y montos ingresados
++ 'dinero': En este array se agregan los ingresos de dinero. Tanto la descripcion como el monto.
+
+## 2._Contadores para conocer el total de gastos o de ingresos:_
++ 'totalGastos': El total de gastos ingresados
++ 'totalIngreso': El total de los ingresos que se ingresaron en el programa.
+
+## 3. _Funciones para agregar ingresos y gastos:_
++ 'agregarSueldo': Permite ingresar el detalle de los ingresos, estos se agregan al array 'dinero'. Actualizando el contador 'totalDinero'
++ 'agregarGasto': Permite ingrear el detalle de los gastos, estos se agregan al array 'gastos'. Actualizando el contador 'totalGastos'
+
+## 4._Funcion para mostrar gastos:_
++ 'mostrarGastos': Muestra uno a uno todos los gastos realizados que estan almacenados en el array 'gastos', al final muestra la sumatoria 'totalGastos'.
+
+## 5._Funcion para mostrar saldo disponible_
++ 'mostrarSaldo': Resta la variable 'totalGastos' a la variable 'totalDinero' y lo imprime .
+
+## 6._Bucle de menu:_
++ Se utiliza un bucle while para mostrar el menu al usuario.
++ Las opciones a elegir son : 1.Ingrsar Dinero - 2.Ingresar Gasto - 3.Mostrar Gastos - 4.Mostrar Saldo - 5.Salir del programa
+
+## 7._Switch:_
++ Utilizamos switch para elegir entre las opciones del menú, cada eleccion realiza la funcion correspondiente.
+
+
+
+
+## _Aca se encuentran los links para conocer el proyecto._
 
 - Link del repositorio de GitHub: https://github.com/AlanMaldonado25/PreEntrega2-Maldonado.git
 - Link Del deployment de GitHub Pages: https://alanmaldonado25.github.io/PreEntrega2-Maldonado/
--- js/index.js
+++ js/index.js
@@ -9,25 +9,39 @@ let totalDinero = 0;
 
 //* Agrega los valores ingresados al array dinero, a su vez aumenta el valor del contador totalDinero
 
-function agregarSueldo(){
-  const descripcion = prompt('Descripcion del ingreso:');
-  const monto = parseFloat(prompt('Ingrese el monto total del deposito:'));
-  
+function agregarSueldo() {
+  const descripcion = prompt('Descripción del ingreso:');
+  let monto;
+
+  do {
+    monto = parseFloat(prompt('Ingrese el monto total del depósito:'));
+    if (isNaN(monto)) {
+      alert('Por favor, ingrese un monto válido.');
+    }
+  } while (isNaN(monto));
+
   dinero.push({ descripcion, monto });
   totalDinero += monto;
-  
-  alert(`Ingresó: ${descripcion}  $${monto.toFixed(2)}`); 
-  }
+
+  alert(`Ingresó: ${descripcion}  $${monto.toFixed(2)}`);
+}
 
 //* Esta funcion agrega los valores ingresados como un gasto al array gastos. Aumenta el valor de totalGastos.
 
 function agregarGasto() {
   const descripcion = prompt('Ingrese la descripción del gasto:');
-  const monto = parseFloat(prompt('Ingrese el monto del gasto:'));
-  
+  let monto;
+
+  do {
+    monto = parseFloat(prompt('Ingrese el monto del gasto:'));
+    if (isNaN(monto)) {
+      alert('Por favor, ingrese un monto válido.');
+    }
+  } while (isNaN(monto));
+
   gastos.push({ descripcion, monto });
   totalGastos += monto;
-  
+
   alert(`Gasto agregado: ${descripcion}  $${monto.toFixed(2)}`);
 }
 
@@ -46,10 +60,15 @@ function mostrarGastos() {
 
 function mostrarSaldo() {
   const saldo = totalDinero - totalGastos;
-  alert(`El saldo disponible es: $${saldo.toFixed(2)}`);
+
+  if (saldo >= 0) {
+    alert(`El saldo disponible es: $${saldo.toFixed(2)}`);
+  } else {
+    alert(`Tu saldo negativo es de: $${saldo.toFixed(2)}`);
+  }
 }
 
-//* Hago las preguntas para que enpieze el programa o termine
+//* Hago las preguntas para que empieze el programa o termine
 
 while (true) {
   const opcion = parseInt(prompt('Seleccione una opción:\n1. Ingresar dinero\n2. Ingresar gasto\n3. Mostrar Gastos\n4.Mostrar Saldo\n5. Salir del programa'));
